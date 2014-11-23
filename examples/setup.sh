9a8c359e25490b8417245c6e4488978dfd4c4441
Makis Tracend <makis.tracend@gmail.com>
2014-11-22 19:05:06 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,14 @@
+# Installing the UniChar CLI
+
+# 1. Install the main executable
+npm install -g unichar-cli
+
+#2. Setup UniChar (only needed once)
+unichar setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+unichar -h
